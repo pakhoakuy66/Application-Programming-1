784584d3dc5eadee27467c69eae16d9e4520e6aa
TranThangBin <thangdev04@gmail.com>
2023-07-18 16:38:15 +0700

    refactor: :art: refactor some code and added multijump system

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -1,63 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float Speed;
+    [SerializeField] private float speed;
     [SerializeField] private CharacterController2D controller;
     [SerializeField] private Animator animator;
 
-    private bool Jump = false;
-    private bool Crouch = false;
+    private bool jump = false;
+    private bool crouch = false;
     private float HorizontalVelocity = 0f;
 
     private void Update()
     {
         HorizontalMovement();
 
-        Jumping();
+        JumpStart();
 
         Crouching();
     }
 
     private void FixedUpdate()
     {
-        controller.Move(HorizontalVelocity * Time.fixedDeltaTime, Crouch, Jump);
-        Jump = false;
+        controller.Move(HorizontalVelocity * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 
     private void HorizontalMovement()
     {
-        HorizontalVelocity = Input.GetAxisRaw("Horizontal") * Speed;
+        HorizontalVelocity = Input.GetAxisRaw("Horizontal") * speed;
         animator.SetFloat("Speed", Mathf.Abs(HorizontalVelocity));
     }
 
-    private void Jumping()
+    private void JumpStart()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            Jump = true;
-            animator.SetBool("IsJumping", true);
+            jump = true;
+            animator.SetBool("Jump", true);
         }
     }
 
+    public void JumpEnd() => animator.SetBool("Jump", false);
     private void Crouching()
     {
         if (Input.GetKey(KeyCode.LeftControl))
-            Crouch = true;
+            crouch = true;
         else if (Input.GetKeyUp(KeyCode.LeftControl))
-            Crouch = false;
+            crouch = false;
     }
 
-    public void CrouchCheck(bool CeilHit)
-    {
-        animator.SetBool("IsCrouching", CeilHit);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(!collision.collider.CompareTag("Player"))
-            animator.SetBool("IsJumping", false);
-    }
+    public void CrouchCheck(bool CeilHit) => animator.SetBool("Crouch", CeilHit);
 }
