49b58567cbd103900bbca044310417690f0a1899
HoangPhuc2709 <phuctran27092004@gmail.com>
2023-08-14 08:26:32 +0700

    feat: :sparkles: bat in lvl2

--- Assets/Scripts/EnemyBAT.cs
+++ Assets/Scripts/EnemyBAT.cs
@@ -2,78 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class EnemyBAT : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-private Transform playerPos;
-private Vector2 currentPos;
-[SerializeField] private float distance;
-[SerializeField] private float speedEnemy;
-private Animator enemyAnim;
+    [SerializeField]
+    private GameObject player;
+    private Transform playerPos;
+    private Vector2 currentPos;
 
+    [SerializeField]
+    private float distance;
 
-private bool isMovingRight = true; // Biến để theo dõi hướng di chuyển của dơi
-private bool isChasingPlayer = false; // Biến để theo dõi trạng thái dơi đuổi theo người chơi
+    [SerializeField]
+    private float speedEnemy;
+    private Animator enemyAnim;
 
-void Start()
-{
-    playerPos = player.GetComponent<Transform>();
-    currentPos = GetComponent<Transform>().position;
-    enemyAnim = GetComponent<Animator>();
-}
+    private bool isMovingRight = true; // Biến để theo dõi hướng di chuyển của dơi
+    private bool isChasingPlayer = false; // Biến để theo dõi trạng thái dơi đuổi theo người chơi
 
-void Update()
-{
-    if (Vector2.Distance(transform.position, playerPos.position) < distance)
+    void Start()
     {
-        if (!isChasingPlayer)
-        {
-            isChasingPlayer = true;
-            Flip();
-        }
-        
-        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speedEnemy * Time.deltaTime);
-        enemyAnim.SetBool("IsFlying", true);
-
-        // Xác định hướng di chuyển của dơi khi đuổi theo người chơi
-        if (transform.position.x < playerPos.position.x && isMovingRight)
-        {
-            isMovingRight = false;
-            Flip();
-        }
-        else if (transform.position.x > playerPos.position.x && !isMovingRight)
-        {
-            isMovingRight = true;
-            Flip();
-        }
+        playerPos = player.GetComponent<Transform>();
+        currentPos = GetComponent<Transform>().position;
+        enemyAnim = GetComponent<Animator>();
     }
-    else
+
+    void Update()
     {
-        if (Vector2.Distance(transform.position, currentPos) <= 0)
+        if (Vector2.Distance(transform.position, playerPos.position) < distance)
         {
-            enemyAnim.SetBool("IsFlying", false);
-            
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, currentPos, speedEnemy * Time.deltaTime);
-            enemyAnim.SetBool("IsFlying", true);
-            if (isChasingPlayer)
+            if (!isChasingPlayer)
             {
-                isChasingPlayer = false;
+                isChasingPlayer = true;
+                Flip();
+            }
+
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                playerPos.position,
+                speedEnemy * Time.deltaTime
+            );
+            enemyAnim.SetBool("IsFlying", true);
+
+            // Xác định hướng di chuyển của dơi khi đuổi theo người chơi
+            if (transform.position.x < playerPos.position.x && isMovingRight)
+            {
+                isMovingRight = false;
+                Flip();
+            }
+            else if (transform.position.x > playerPos.position.x && !isMovingRight)
+            {
+                isMovingRight = true;
                 Flip();
             }
         }
+        else
+        {
+            if (Vector2.Distance(transform.position, currentPos) <= 0)
+            {
+                enemyAnim.SetBool("IsFlying", false);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    currentPos,
+                    speedEnemy * Time.deltaTime
+                );
+                enemyAnim.SetBool("IsFlying", true);
+                if (isChasingPlayer)
+                {
+                    isChasingPlayer = false;
+                    Flip();
+                }
+            }
+        }
     }
-}
 
-void Flip()
-{
-    Vector3 scale = transform.localScale;
-    scale.x *= -1;
-    transform.localScale = scale;
-}
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.TryGetComponent<PlayerController>(out var player))
+            player.Die();
+    }
 }
