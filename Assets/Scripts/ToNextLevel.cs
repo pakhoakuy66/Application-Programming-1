e9efe32ffe307076caec5e4dfbb5589a9706b6a7
TranThangBin <thangdev04@gmail.com>
2023-08-19 13:04:36 +0700

    chore: :memo: moving folders around and door move to next level

---
+++ Assets/Scripts/ToNextLevel.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToNextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent<PlayerController>(out _))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
